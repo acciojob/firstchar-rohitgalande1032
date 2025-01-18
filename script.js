0dd2f0794c8d51dfffaca442f720bc8f1c76908c
Rohit Galande <80148975+rohitgalande1032@users.noreply.github.com>
2025-01-18 23:07:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-function firstChar(text) {
-  // your code here
+function firstChar(str) {
+  if(str === " "){
+    return `${" "}`;
+  }
+  
+  if(str === ""){
+    return `${''}`;
+  }
+  
+  let str1 = str.trim();
+  return str1[0]
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+const text = prompt("Enter text:");
 alert(firstChar(text));
